8822bba07ad9b6c3e212433d934e1de79fe5f30b
HaiPhu2702 <phukaht@gmail.com>
2022-09-27 06:27:44 +0700

    project-test

---
+++ src/config/db.js
@@ -0,0 +1,14 @@
+const mysql = require('mysql2');
+const util = require("util");
+
+const connection = mysql.createConnection({
+    host     : process.env.HOST,
+    user     : process.env.USER,
+    password : process.env.PASSWORD,
+    database : process.env.DATABASE
+});
+
+const query = util.promisify(connection.query).bind(connection);
+const connect = util.promisify(connection.connect).bind(connection);
+
+module.exports = {query,connect}
---
+++ src/controller/city.js
@@ -0,0 +1,21 @@
+
+
+class CityController{
+    constructor(){}
+
+    async listCity(req, res, next){
+     const sql=`select * from city`
+
+    }
+      async addACity(req, res, next){
+
+    }
+      async updateACity(req, res, next){
+
+    }
+      async deleteACity(req, res, next){
+
+    }
+}
+
+module.exports =new CityController()
